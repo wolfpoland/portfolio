58bff618fd8b13c59ed54b95523c1faaa461c2d9
Patryk Krasuski <krasuskipatryk94@gmail.com>
2023-10-12 14:12:07 +0200

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import { ThemeProvider } from "patryk/components/theme-provider";
+import {ThemeProvider} from "patryk/components/theme-provider";
 import "./globals.css";
-import { Toaster } from "patryk/components/ui/toaster";
+import {Toaster} from "patryk/components/ui/toaster";
+import { Analytics } from '@vercel/analytics/react'
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <html className="w-full h-full" lang="en" suppressHydrationWarning>
-      <link rel="icon" href="/beza.png" sizes="any" />
-      <body className="w-full h-full">
+    return (
+        <html className="w-full h-full" lang="en" suppressHydrationWarning>
+        <link rel="icon" href="/beza.png" sizes="any"/>
+        <body className="w-full h-full">
         <ThemeProvider attribute="class" defaultTheme="dark">
-          {children}
+            {children}
+            <Analytics/>
         </ThemeProvider>
-        <Toaster />
-      </body>
-    </html>
-  );
+        <Toaster/>
+        </body>
+
+        </html>
+    );
 }
